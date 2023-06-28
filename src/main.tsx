1b7456ed2c466f92c85fdb6d32ca68bec5b4f342
Markus Lund <markuslund92@gmail.com>
2023-06-28 14:03:23 +0200

    Create PWA

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/sw.js").then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ workbox-config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  globDirectory: "dist/",
+  globPatterns: ["**/*.{html,js,css,svg,png}"],
+  swDest: "dist/sw.js",
+  swSrc: "src/sw.js",
+};
